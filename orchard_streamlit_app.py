6966b331f49c8d01ce93b79720ef031452fa17ff
Meuhyeuh <marianne.lmbrt@gmail.com>
2025-06-15 15:13:36 -0400

    Update orchard_streamlit_app.py

--- orchard_streamlit_app.py
+++ orchard_streamlit_app.py
@@ -101,6 +101,20 @@ def generate_orchard():
 
 if st.button("Simulate Orchard"):
     grid = generate_orchard()
+
+    species_counts = {s["name"]: 0 for s in SPECIES}
+    for i in range(num_rows):
+        for j in range(num_cols):
+            species_name = ID_TO_NAME[grid[i, j]]
+            species_counts[species_name] += 1
+
+    total_trees = sum(species_counts.values())
+    species_df = pd.DataFrame.from_dict(species_counts, orient='index', columns=['Tree Count'])
+    species_df['Percentage'] = (species_df['Tree Count'] / total_trees * 100).round(1).astype(str) + '%'
+
+    st.subheader("Tree Counts by Species")
+    st.write(species_df)
+    
     fig, ax = plt.subplots(figsize=(12, 2.5))
     color_array = np.vectorize(COLOR_MAP.get)(grid)
     ax.imshow(grid, cmap=None)
